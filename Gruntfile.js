a51e9063e7dd9c41f1c5bf6386032ca62cf7482f
Kiattipong Kamonrat <kiattipong@thaibma.or.th>
2022-01-14 14:18:17 +0700

    add more grunt support

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,9 @@ module.exports = (grunt) => {
   const sass = require("node-sass");
 
   require("time-grunt")(grunt);
-  require("jit-grunt")(grunt);
+  require("jit-grunt")(grunt, {
+    useminPrepare: "grunt-usemin",
+  });
 
   grunt.initConfig({
     sass: {
@@ -35,8 +37,123 @@ module.exports = (grunt) => {
         },
       },
     },
+    copy: {
+      html: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "./",
+            src: ["*.html"],
+            dest: "dist",
+          },
+        ],
+      },
+    },
+    clean: {
+      build: {
+        scr: ["dist/"],
+      },
+    },
+    imagemin: {
+      dynamic: {
+        files: [
+          {
+            expand: true,
+            dot: true,
+            cwd: "./",
+            src: ["img/*.{png,jpg,gif}"],
+            dest: "dist/",
+          },
+        ],
+      },
+    },
+    useminPrepare: {
+      foo: {
+        dest: "dist",
+        src: ["index.html", "aboutus.html", "contactus.html"],
+      },
+      options: {
+        flow: {
+          steps: {
+            css: ["cssmin"],
+            js: ["uglify"],
+          },
+          post: {
+            css: [
+              {
+                name: "cssmin",
+                createConfig: (context, bloxk) => {
+                  const generated = context.options.generated;
+                  generated.options = {
+                    keepSpecialComments: 0,
+                    rebase: false,
+                  };
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+    concat: {
+      options: {
+        separator: ";",
+      },
+      dist: {},
+    },
+    uglify: {
+      dist: {},
+    },
+    cssmin: {
+      dist: {},
+    },
+    filerev: {
+      options: {
+        encoding: "utf8",
+        algorithm: "md5",
+        length: 20,
+      },
+      releases: {
+        files: [
+          {
+            src: ["dist/js/*.js", "dist/css/*.css"],
+          },
+        ],
+      },
+    },
+    usemin: {
+      html: ["dist/index.html", "dist/aboutus.html", "dist/contactus.html"],
+      options: {
+        assetsDirs: ["dist", "dist/css", "dist/js"],
+      },
+    },
+    htmlmin: {
+      dist: {
+        options: {
+          collapseWhitespace: true,
+        },
+        files: {
+          "dist/index.html": "dist/index.html",
+          "dist/aboutus.html": "dist/aboutus.html",
+          "dist/contactus.html": "dist/contactus.html",
+        },
+      },
+    },
   });
 
   grunt.registerTask("css", ["sass"]);
   grunt.registerTask("default", ["browserSync", "watch"]);
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "imagemin",
+    "useminPrepare",
+    "concat",
+    "cssmin",
+    "uglify",
+    "filerev",
+    "usemin",
+    "htmlmin",
+  ]);
 };
